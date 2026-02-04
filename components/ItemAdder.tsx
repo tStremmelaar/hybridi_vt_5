0fdee91b0db588fa5f6816e573189e5a55dbab16
Thomas Stremmelaar <t.stremmie@gmail.com>
2026-02-04 15:31:29 +0200

    added submit button

--- components/ItemAdder.tsx
+++ components/ItemAdder.tsx
@@ -1,9 +1,10 @@
-import { TextInput, View } from "react-native";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
 import { itemAdderProps } from "../types";
-import { useState } from "react";
+import { Ref, useRef, useState } from "react";
 
 export default function ItemAdder({props}: {props: itemAdderProps}) {
   const [text, setText] = useState<string>('')
+  const inputRef = useRef<TextInput>(null)
 
   function submit() {
     props.add(text)
@@ -11,15 +12,42 @@ export default function ItemAdder({props}: {props: itemAdderProps}) {
   }
 
   return(
-    <View>
+    <View style={styles.line}>
       <TextInput
-        enterKeyHint="done"
+        ref={inputRef}
         maxLength={300}
+        multiline={true}
         onChangeText={setText}
         onSubmitEditing={submit}
         placeholder="Enter task"
+        returnKeyType="done"
         value={text}
+        style={styles.input}
+        submitBehavior="blurAndSubmit"
       />
+      <Pressable onPress={() => {
+        submit()
+        inputRef.current?.blur()
+      }}>
+        <Text style={styles.button}>Save</Text>
+      </Pressable>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  line: {
+    flexDirection: "row",
+  },
+  input: {
+    flex: 3,
+    margin: 10,
+    fontSize: 20,
+  },
+  button: {
+    flex: 1,
+    margin: 10,
+    color: "#08f",
+    fontSize: 24
+  },
+})
